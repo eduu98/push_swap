bfbeaa99e69c446a6d17bf078226c2297eb5b350
Eduardo Cruz gomez-chacon <ecruz-go@c3r3s1.42madrid.com>
2021-09-13 12:50:58 +0200

    cambios en la logica interna

--- internal_logic.c
+++ internal_logic.c
@@ -1,33 +1,11 @@
 #include "push_swap.h"
 
-int	check_for_max_min(t_elem *stack, int max)
-{
-	int aux;
-
-	if (stack)
-	{
-		while (stack->next)
-		{
-			if (!aux || (max && aux < stack->value) || (!max && aux > stack->value))
-				aux = stack->value;
-			stack = stack->next;
-		}
-		if (!aux || (max && aux < stack->value) || (!max && aux > stack->value))
-			aux = stack->value;
-	}
-	return (aux);
-}
-
 void	order_three(t_elem *stack)
 {
-	// buscar el maximo y minimo
-	int max;
-	int min;
-
-	max = check_for_max_min(stack, 1);
-	min = check_for_max_min(stack, 0);
-
-
-	// comprobar si el maximo esta abajo
-
+	// comprobar si el minimo esta arriba
+	if (stack->index != 0)
+	{
+		stack = stack->next;
+		 
+	}
 }
